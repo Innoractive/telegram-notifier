021752333b02a4265816d8a25b0371e728add5b9
kch <wilzokch@gmail.com>
2019-04-30 16:32:31 +0800

    - auto load env config

--- install.php
+++ install.php
@@ -2,9 +2,6 @@
 // Load composer
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::create(__DIR__);
-$dotenv->load();
-
 $botApiKey = getenv('TELEGRAM_API');
 $botName   = getenv('TELEGRAM_BOTNAME');
 $hookUrl   = getenv('TELEGRAM_WEBHOOK_URL');
