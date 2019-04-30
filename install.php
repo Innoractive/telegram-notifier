554d39880910879e0ddbf76a384024ee58677492
kch <wilzokch@gmail.com>
2019-04-30 16:03:34 +0800

    - add install webhook script

---
+++ install.php
@@ -0,0 +1,25 @@
+<?php
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
+$botApiKey = getenv('TELEGRAM_API');
+$botName   = getenv('TELEGRAM_BOTNAME');
+$hookUrl   = getenv('TELEGRAM_WEBHOOK_URL');
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($botApiKey, $botName);
+
+    // Set webhook
+    $result = $telegram->setWebhook($hookUrl);
+	
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // log telegram errors
+    // echo $e->getMessage();
+}
