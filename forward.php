7ee97f49e410c01a5bc3394f7e91abd939293b09
kch <wilzokch@gmail.com>
2019-06-10 17:28:48 +0800

    - update forwarder

--- forward.php
+++ forward.php
@@ -3,4 +3,14 @@
 // Load composer
 require __DIR__ . '/vendor/autoload.php';
 
-// TODO - forward mail to telegram
+// forward mail to telegram
+if(isset($_POST['recipient'])) {
+    list($to, $domail) = explode('@', trim($_POST['recipient'], '<>'));
+
+    \Longman\TelegramBot\Request::sendMessage([
+        'chat_id' => $to,
+        'parse_mode' => 'HTML',
+        'text' => $_POST['stripped-text'],
+        'disable_web_page_preview' => true,
+    ]);
+}
