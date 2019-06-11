8ac2adfc734a068727d3e538573d815b2cd633ff
kch <wilzokch@gmail.com>
2019-06-11 13:13:13 +0800

    - add index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo getenv('TELEGRAM_BOTNAME');?> | Telegram Notifier for Innoractive.com</title>
+    <link href="https://unpkg.com/nes.css@2.2.0/css/nes.min.css" rel="stylesheet" />
+</head>
+<body>
+
+</body>
+</html>
