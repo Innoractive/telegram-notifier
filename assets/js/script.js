789b29e767961ab99090b5db388e75c6a7db699f
kch <wilzokch@gmail.com>
2019-06-11 15:09:35 +0800

    - validate chat id

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,29 +8,45 @@ document.addEventListener('scroll', () => {
 });
 
 function copytext() {
-  var text = document.getElementById('telegram-email').value;
+  var email = document.getElementById('telegram-email').value;
   var copyFrom = document.getElementById('copy-textarea');
   if(!copyFrom) {
     copyFrom = document.createElement('textarea');
     copyFrom.id = 'copy-textarea';
     copyFrom.style = 'position: absolute;left:-1000px;top:-1000px;';
-    copyFrom.innerHTML = text;
+    copyFrom.innerHTML = email;
     document.querySelector('body').appendChild(copyFrom);
   } else {
-    copyFrom.innerHTML = text;
+    copyFrom.innerHTML = email;
   }
 
   copyFrom.select();
   document.execCommand('copy');
 
-  $('#email-link').text(text);
-  $('#email-link').attr('href', 'mailto:' + text);
-  $('#last-step').show();
+  if(email == '' || email == mail_domain) {
+    $('#chat-id').addClass('is-error');
+    $('#last-step').hide();
+  } else {
+    $('#chat-id').removeClass('is-error');
+    $('#email-link').text(email);
+    $('#email-link').attr('href', 'mailto:' + email);
+    $('#last-step').show();
+  }
 }
 
 $(document).ready(function() {
+  $('#chat-id').click(function() {
+    $(this).val('');
+    $('#last-step').hide();
+    $(this).keyup();
+  });
   $('#chat-id').keyup(function() {
-    $('#telegram-email').val($(this).val() + mail_domain);
+    if($(this).val() != '') {
+      $('#chat-id').removeClass('is-error');
+      $('#telegram-email').val($(this).val() + mail_domain);
+    } else {
+      $('#telegram-email').val('');
+    }
   });
   $('#copy-btn').click(copytext);
 });
