16b5df87b72ac9e26f1d6e78b990e9be37ded77a
kch <wilzokch@gmail.com>
2020-04-08 11:35:23 +0800

    - improved regex for trim whitespace

--- forward.php
+++ forward.php
@@ -16,8 +16,7 @@ if(isset($_POST['recipient'])) {
     $type = getChannelType($_POST['subject'] . ' ' . $_POST['stripped-text']);
 
     // trim whitespace in first char
-    $_POST['stripped-text'] = preg_replace("/[\t ]+/", ' ', $_POST['stripped-text']);
-    $_POST['stripped-text'] = preg_replace("/[\n]+[\n ]* /", "\n", $_POST['stripped-text']);
+    $_POST['stripped-text'] = preg_replace("/[\n]+[\n\t ]*[\t ]/", "\n", $_POST['stripped-text']);
 
     $message = sprintf(file_get_contents('view/telegram-template.html'),
         getIcon($type),
